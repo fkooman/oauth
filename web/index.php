851585357c13fc497953c0b35433d6522cbde097
François Kooman <fkooman@tuxed.net>
2015-07-21 13:22:45 +0200

    use external fkooman/oauth library now

--- web/index.php
+++ web/index.php
@@ -2,11 +2,8 @@
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-use fkooman\Rest\Service;
 use fkooman\Rest\Plugin\Authentication\AuthenticationPlugin;
-use fkooman\Rest\Plugin\Authentication\UserInfoInterface;
 use fkooman\Rest\Plugin\Authentication\Basic\BasicAuthentication;
-use fkooman\Http\Request;
 use fkooman\OAuth\OAuthServer;
 use fkooman\OAuth\Impl\TwigTemplateManager;
 use fkooman\OAuth\Impl\CryptoAuthorizationCode;
@@ -16,16 +13,14 @@ use fkooman\Json\Json;
 use fkooman\Ini\IniReader;
 use fkooman\OAuth\Impl\NoRegistrationClient;
 use fkooman\OAuth\Impl\JsonResourceServer;
+use fkooman\OAuth\OAuthService;
 
+// CONFIG
 $iniReader = IniReader::fromFile(
     dirname(__DIR__).'/config/server.ini'
 );
 
-$service = new Service();
-
-$jsonResourceServer = new JsonResourceServer(dirname(__DIR__).'/config/resource_servers.json');
-
-// for user authentication
+// USER AUTH
 $userAuthentication = new BasicAuthentication(
     function ($userId) {
         $c = Json::decodeFile(dirname(__DIR__).'/config/users.json');
@@ -37,11 +32,10 @@ $userAuthentication = new BasicAuthentication(
     )
 );
 
-#$userAuthentication = new IndieAuthAuthentication();
-
-// for resource server authentication
+// RESOURCE SERVER AUTH
 $resourceServerAuthentication = new BasicAuthentication(
-    function ($resourceServerId) use ($jsonResourceServer) {
+    function ($resourceServerId) {
+        $jsonResourceServer = new JsonResourceServer(dirname(__DIR__).'/config/resource_servers.json');
         $resourceServer = $jsonResourceServer->getResourceServer($resourceServerId);
         if (false === $resourceServer) {
             return false;
@@ -54,73 +48,20 @@ $resourceServerAuthentication = new BasicAuthentication(
     )
 );
 
-$authenticationPlugin = new AuthenticationPlugin();
-$authenticationPlugin->register($userAuthentication, 'user');
-$authenticationPlugin->register($resourceServerAuthentication, 'resource_server');
-
-//$pluginRegistry = new PluginRegistry();
-//$pluginRegistry->registerDefaultPlugin($authenticationPlugin);
-
-$service->getPluginRegistry()->registerDefaultPlugin($authenticationPlugin);
-
 $key = Key::load($iniReader->v('Security', 'Key'));
-
 $o = new OAuthServer(
     new TwigTemplateManager(),
     new NoRegistrationClient(),
-    $jsonResourceServer,
+    new JsonResourceServer(dirname(__DIR__).'/config/resource_servers.json'),
     new CryptoAuthorizationCode($key),
     new CryptoAccessToken($key)
 );
 
-$service->get(
-    '/authorize',
-    function (Request $request, UserInfoInterface $userInfo) use ($o) {
-        return $o->getAuthorize($request, $userInfo);
-    },
-    array(
-        'fkooman\Rest\Plugin\Authentication\AuthenticationPlugin' => array(
-            'only' => 'user',
-        ),
-    )
-);
+$service = new OAuthService($o);
 
-$service->post(
-    '/authorize',
-    function (Request $request, UserInfoInterface $userInfo) use ($o) {
-        return $o->postAuthorize($request, $userInfo);
-    },
-    array(
-        'fkooman\Rest\Plugin\Authentication\AuthenticationPlugin' => array(
-            'only' => 'user',
-        ),
-    )
-
-);
-
-$service->post(
-    '/token',
-    function (Request $request) use ($o) {
-        return $o->postToken($request);
-    },
-    array(
-        'fkooman\Rest\Plugin\Authentication\AuthenticationPlugin' => array(
-            //'only' => 'client',
-            'enabled' => false,
-        ),
-    )
-);
-
-$service->post(
-    '/introspect',
-    function (Request $request, UserInfoInterface $userInfo) use ($o) {
-        return $o->postIntrospect($request, $userInfo);
-    },
-    array(
-        'fkooman\Rest\Plugin\Authentication\AuthenticationPlugin' => array(
-            'only' => 'resource_server',
-        ),
-    )
-);
-
+// AUTHENTICATION PLUGIN
+$authenticationPlugin = new AuthenticationPlugin();
+$authenticationPlugin->register($userAuthentication, 'user');
+$authenticationPlugin->register($resourceServerAuthentication, 'resource_server');
+$service->getPluginRegistry()->registerDefaultPlugin($authenticationPlugin);
 $service->run()->send();
